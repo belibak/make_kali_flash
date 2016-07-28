cd14e424878ca88d2b671e9b3b183f72c1dc29e3
Alexander Rusetski <alex.rus@core-tech.ru>
2016-07-28 14:09:12 +0300

    add files

--- mk_crypt_partition.sh
+++ mk_crypt_partition.sh
@@ -1,1 +1,20 @@
-/home/white/bin/mk_crypt_partition.sh
+#!/bin/bash
+if [ -z $1 ]
+then
+	sudo fdisk -l
+	echo "Enter device where make a new partition. like /dev/sdb3"
+	read dev
+else
+	dev=$1
+fi
+crypt_usb="crypt_usb"
+#sudo fdisk "$dev"
+cryptsetup --verbose --verify-passphrase luksFormat "$dev"
+cryptsetup luksOpen "$dev" $crypt_usb
+mkfs.ext3 /dev/mapper/$crypt_usb
+e2label /dev/mapper/$crypt_usb persistence
+mkdir -p /mnt/$crypt_usb
+mount /dev/mapper/$crypt_usb /mnt/$crypt_usb
+echo "/ union" > /mnt/$crypt_usb/persistence.conf
+umount /dev/mapper/$crypt_usb
+cryptsetup luksClose /dev/mapper/$crypt_usb
